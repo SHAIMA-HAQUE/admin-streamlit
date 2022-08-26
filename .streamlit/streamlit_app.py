f30c07d7f77656f120bb6fa28641ae4e3f37eb4c
Shaima Haque <shaimahaque02@yahoo.com>
2022-08-26 09:46:11 +0530

    Change secrets.toml into .streamlit

--- .streamlit/streamlit_app.py
+++ .streamlit/streamlit_app.py
@@ -3,6 +3,25 @@
 import streamlit as st
 import pymongo
 
+
+import streamlit as st
+
+# Everything is accessible via the st.secrets dict:
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
+
+
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
 @st.experimental_singleton
