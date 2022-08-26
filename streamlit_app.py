1744d07558f606e8885a3fafe507cdae6da148c1
Shaima Haque <shaimahaque02@yahoo.com>
2022-08-26 09:29:56 +0530

    Add streamlit_app.py and requirements.txt

---
+++ streamlit_app.py
@@ -0,0 +1,27 @@
+# streamlit_app.py
+
+import streamlit as st
+import pymongo
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["mongo"])
+
+client = init_connection()
+
+# Pull data from the collection.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def get_data():
+    db = client.mydb
+    items = db.mycollection.find()
+    items = list(items)  # make hashable for st.experimental_memo
+    return items
+
+items = get_data()
+
+# Print results.
+for item in items:
+    st.write(f"{item['name']} has a :{item['pet']}:")
